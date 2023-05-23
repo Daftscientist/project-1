b8d698918343326233555f27a729dfb46834a226
daftscientist <leojohnston12032@gmail.com>
2023-05-23 23:34:00 +0100

    added an avatar column to the user table

--- backend/database/models/user.py
+++ backend/database/models/user.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Uuid, DateTime
+from sqlalchemy import Column, Integer, String, Uuid, DateTime, URL
 import uuid
 from database.db import Base
 from sqlalchemy.sql import func
@@ -12,6 +12,7 @@ class User(Base):
     username = Column(String, nullable=False)
     email = Column(String, nullable=False)
     password = Column(String, nullable=False)
+    avatar = Column(String, nullable=True, default=None)
     created_at = Column(DateTime(timezone=True), nullable=False)
     google_account_identifier = Column(String(255), nullable=True, default=None)
     discord_account_identifier = Column(String(18), nullable=True, default=None)
