70df137c37f630f80193d1706cbbc638974dbca4
daftscientist <leojohnston12032@gmail.com>
2023-05-23 18:12:01 +0100

    cookie part finished - can't do more until cache is built

--- backend/api/user/logout.py
+++ backend/api/user/logout.py
@@ -1,14 +1,17 @@
-from sanic import Request, text
+from sanic import Request, text, BadRequest
+from core.cookies import check_if_cookie_is_present, remove_cookie
 from core.responses import Success
 
 async def logout_route(request: Request):
     """ The logout route. """
 
-    ## check if the user is logged in
-
-    ## delete the cookie
-
-    ## delete the session info from cache
+    ## check if cookies are present
+    if not await check_if_cookie_is_present(request):
+        return await BadRequest(request, "You are not logged in.")
+    
+    ## check if session info is present in cache
 
     response = Success(request, "Logged out successfully.")
+    await remove_cookie(response)
+    ## delete the session info from cache
     return response
