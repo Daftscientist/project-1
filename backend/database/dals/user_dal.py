4dd8871500130649e48a454aef6f8d7ab179e5cb
daftscientist <leojohnston12032@gmail.com>
2023-05-23 18:02:50 +0100

    added function to check user existance

--- backend/database/dals/user_dal.py
+++ backend/database/dals/user_dal.py
@@ -11,19 +11,26 @@ class UsersDAL():
         self.db_session = db_session
 
     async def create_user(self, username: str, email: str, password: str):
+        """Creates a new user."""
         new_user = User(username=username, email=email, password=password)
         self.db_session.add(new_user)
         await self.db_session.flush()
 
     async def get_user_by_uuid(self, uuid: int) -> User:
+        """Returns the user with the given uuid."""
+
         q = await self.db_session.execute(select(User).where(User.uuid == uuid))
         return q.scalars().first()
 
     async def get_all_users(self) -> List[User]:
+        """Returns all users."""
+
         q = await self.db_session.execute(select(User).order_by(User.identifier))
         return q.scalars().all()
 
     async def update_user(self, uuid: int, username: Optional[str] = None, email: Optional[str] = None, password: Optional[str] = None, google_account_identifier: Optional[str] = None, discord_account_identifier: Optional[str] = None):
+        """Updates the user with the given uuid."""
+        
         q = update(User).where(User.uuid == uuid)
         if username:
             q = q.values(username=username)
@@ -37,3 +44,13 @@ class UsersDAL():
             q = q.values(discord_account_identifier=discord_account_identifier)
         q.execution_options(synchronize_session="fetch")
         await self.db_session.execute(q)
+    
+    async def check_if_user_exists(self, username: str, email: str) -> bool:
+        """Checks if a user with the given username or email exists."""
+        q = await self.db_session.execute(select(User).where(User.username == username))
+        if q.scalars().first():
+            return True
+        q = await self.db_session.execute(select(User).where(User.email == email))
+        if q.scalars().first():
+            return True
+        return False
