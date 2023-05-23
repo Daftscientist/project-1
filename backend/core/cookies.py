78d030b8c7f63e838b694f678d5d6859960f2d3a
daftscientist <leojohnston12032@gmail.com>
2023-05-23 18:06:04 +0100

    removed wrong type checks

--- backend/core/cookies.py
+++ backend/core/cookies.py
@@ -1,11 +1,10 @@
-from sanic import json
 import jwt
 
 SECRET_KEY = '23893784023409283964732894790792848932798479012043789247589357838401293890' ## For testing only. This should be stored in an environment variable.
 ALGORITHM = 'HS256'
 COOKIE_IDENTITY = 'session'
 
-async def set_cookie(response: json, data: dict):
+async def set_cookie(response, data: dict):
     """Sets the cookie in the response."""
     response.add_cookie(
         COOKIE_IDENTITY,
@@ -14,15 +13,15 @@ async def set_cookie(response: json, data: dict):
     )
     return response
 
-async def get_cookie(request: json):
+async def get_cookie(request):
     """Gets the cookie from the request."""
     return jwt.decode(request.cookies.get(COOKIE_IDENTITY), SECRET_KEY, algorithms=[ALGORITHM])
 
-async def remove_cookie(response: json):
+async def remove_cookie(response):
     """Removes the cookie from the response."""
     response.del_cookie(COOKIE_IDENTITY)
     return response
 
-async def check_if_cookie_is_present(request: json):
+async def check_if_cookie_is_present(request):
     """Checks if the cookie is present."""
     return COOKIE_IDENTITY in request.cookies
