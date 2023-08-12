5e4f8844aa7aa02390f3500bd9bc296f945218bb
daftscientist <leojohnston12032@gmail.com>
2023-08-12 11:15:10 +0100

    removing stuff

--- backend/testtsts/plugins/plugin1.py
+++
@@ -1,7 +0,0 @@
-from yapsy.IPlugin import IPlugin
-from yapsy.PluginInfo import PluginInfo
-
-class PluginOne(IPlugin):
-    def print_name(self):
-        
-        print("This is plugin 1", PluginInfo.plugin_name)
