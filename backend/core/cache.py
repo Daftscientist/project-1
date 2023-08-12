e5a5ff05bcab506706635684481eb6bea63c46ac
daftscientist <leojohnston12032@gmail.com>
2023-08-12 20:00:25 +0100

    adding session ids to the cache

--- backend/core/cache.py
+++ backend/core/cache.py
@@ -1,5 +1,6 @@
 from database import db
 from database.dals.user_dal import UsersDAL
+from typing import Union
 
 CACHED_DATA = dict()
 CACHED_EMAIL_TO_UUID = dict()
@@ -13,12 +14,12 @@ async def init():
             users_dal = UsersDAL(session)
             users = await users_dal.get_all_users()
             for user in users:
-                await add(user.identifier, user.uuid, user.email, user.username, user.avatar, user.google_account_identifier, user.discord_account_identifier, user.created_at)
+                await add(user.identifier, user.uuid, user.email, user.username, user.avatar, user.google_account_identifier, user.discord_account_identifier, user.created_at, None)
     print("Cache initilised.")
     print("Cache contains " + str(len(CACHED_DATA)) + " users.")
 
 
-async def add(identifier: str, uuid: str, email: str, username: str, avatar: str, google_account_identifier: str, discord_account_identifier: str, created_at: str) -> dict:
+async def add(identifier: str, uuid: str, email: str, username: str, avatar: str, google_account_identifier: str, discord_account_identifier: str, created_at: str, session_id: Union[str, None]) -> dict:
     ''' adds a user to the cache '''
     CACHED_DATA[uuid] = {
         "identifier": identifier,
@@ -28,7 +29,8 @@ async def add(identifier: str, uuid: str, email: str, username: str, avatar: str
         "avatar": avatar,
         "google_account_identifier": google_account_identifier,
         "discord_account_identifier": discord_account_identifier,
-        "created_at": created_at
+        "created_at": created_at, 
+        "session_id": session_id
     }
     CACHED_EMAIL_TO_UUID[email] = uuid
     return CACHED_DATA[uuid]
@@ -65,7 +67,7 @@ async def clear() -> None:
 async def get_all() -> list[dict]:
     return list(CACHED_DATA.values())
 
-async def update(uuid: str, email: str = None, username: str = None, avatar: str = None, google_account_identifier: str = None, discord_account_identifier: str = None) -> dict:
+async def update(uuid: str, email: str = None, username: str = None, avatar: str = None, google_account_identifier: str = None, discord_account_identifier: str = None, session_id: Union[str, None] = None) -> dict:
     if email:
         del CACHED_EMAIL_TO_UUID[CACHED_DATA[uuid]["email"]]
         CACHED_DATA[uuid]["email"] = email
@@ -78,4 +80,6 @@ async def update(uuid: str, email: str = None, username: str = None, avatar: str
         CACHED_DATA[uuid]["google_account_identifier"] = google_account_identifier
     if discord_account_identifier:
         CACHED_DATA[uuid]["discord_account_identifier"] = discord_account_identifier
+    if session_id:
+        CACHED_DATA[uuid]["session_id"] = session_id
     return CACHED_DATA[uuid]
--- backend/views/user/create.py
+++ backend/views/user/create.py
@@ -6,7 +6,7 @@ from database import db
 from core import encoder
 from core.responses import Success
 from core.cookies import check_if_cookie_is_present
-from core.cache import add, CACHED_DATA
+from core.cache import add
 
 EMAIL_REGEX = re.compile(r"^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$")
 
@@ -55,6 +55,5 @@ class CreateView(HTTPMethodView):
                 user_info = await users_dal.get_user_by_email(params.email)
 
                 await add(user_info.identifier, user_info.uuid, user_info.email, user_info.username, user_info.avatar, user_info.google_account_identifier, user_info.discord_account_identifier, user_info.created_at)
-                print(CACHED_DATA)
 
         return await Success(request, "User created successfully.")
