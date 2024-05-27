69f054b3b0b39451cf898b692742d48314db3086
Leo Johnston <67394753+Daftscientist@users.noreply.github.com>
2024-05-27 14:37:22 +0000

    finished encryption functions

--- backend/core/encryption.py
+++ backend/core/encryption.py
@@ -1,11 +1,34 @@
 from cryptography.fernet import Fernet
+import sanic
 
-fernet = Fernet()
+fernet = Fernet(
+    bytes(
+        sanic.Sanic.get_app().config["core"]["encryption_key"],
+        "utf-8"
+    )
+)
 
 def encrypt(string: str) -> bytes:
-    """ Takes a string and uses the key provided to encrypt the string. """
+    """
+    Encrypts a string using the Fernet encryption algorithm.
+
+    Args:
+        string (str): The string to be encrypted.
+
+    Returns:
+        bytes: The encrypted string as bytes.
+    """
     return fernet.encrypt(string.encode())
 
 def decrypt(bytes: bytes) -> str:
-    """ Takes bytes and uses the key provided to decrypt the string. """
+    """
+    Decrypts the given bytes using the Fernet encryption algorithm.
+
+    Args:
+        bytes (bytes): The encrypted bytes to be decrypted.
+
+    Returns:
+        str: The decrypted string.
+
+    """
     return fernet.decrypt(bytes).decode()
